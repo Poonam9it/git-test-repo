2e09bbd057b4f6e8182613eb630db3cf8387a310
Sangramsing Patil <sangramsing@obo.pm>
2018-12-10 00:12:57 +0530

    Formatted DemoApp class and updated index page for welcome message.

--- jsp-servlet-app/src/main/java/com/webapp/demo/DemoApp.java
+++ jsp-servlet-app/src/main/java/com/webapp/demo/DemoApp.java
@@ -2,6 +2,6 @@ package com.webapp.demo;
 
 public class DemoApp {
 	public static void main(String[] args) {
-System.out.println("Demo web application ");
+		System.out.println("Demo web application ");
 	}
 }
--- jsp-servlet-app/src/main/webapp/index.jsp
+++ jsp-servlet-app/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<h3 style="color: orange;">Welcome to Java Web Application  </h3>
 </body>
 </html>
